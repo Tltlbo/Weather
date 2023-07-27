752a82f34cf8530c9b2caced1722045cf415a7f2
TLtlbo <js1436kt@gmail.com>
2023-07-28 00:07:38 +0900

    API호출로 가져온 값들을 버튼을 누르면 각 라벨에 매핑 되게 구현 및 잘못된 도시 이름이 입력되면 API에서 보내온 에러 메세지가 Alert를 통해 출력 되도록 구현

---
+++ Weather/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by 박진성 on 2023/07/27.
+//
+
+import Foundation
+
+struct ErrorMessage : Codable {
+    let message : String
+}
--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var minTempLabel: UILabel!
     
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     
     override func viewDidLoad() {
@@ -35,14 +36,42 @@ class ViewController: UIViewController {
         }
     }
     
+    func configureView(weatherInformation : WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDiscription.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))°C" //섭씨 변환
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))°C"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))°C"
+    }
+    
+    func showAlert(errorMessage : String) {
+        let alert = UIAlertController(title: "에러", message: errorMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func getCurrentWeather(cityName : String) {
         guard let url = URL(string : "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=8ddbb342fa736fddf4b75f66ec875df9") else {return}
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
+            let successRange = (200 ..< 300)
             guard let data = data, error == nil else {return}
             let decoder = JSONDecoder()
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
-            debugPrint(weatherInformation)
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else {return}
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else {return}
+                DispatchQueue.main.async {
+                    self?.showAlert(errorMessage: errorMessage.message)
+                }
+            }
+            
         }.resume()
     }
 }
