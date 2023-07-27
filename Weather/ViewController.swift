86ca8a17574a102c48ca78d8333a8c1c56a5482c
TLtlbo <js1436kt@gmail.com>
2023-07-27 23:48:36 +0900

    API를 호출 하여 요청된 도시의 날씨 정보 JSON을 decode하여 구조체에 매핑 기능 구현 및 출력 구현

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var weatherDiscription: UILabel!
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
+    @IBOutlet weak var minTempLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tapWeatherFetchButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(cityName : String) {
+        guard let url = URL(string : "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=8ddbb342fa736fddf4b75f66ec875df9") else {return}
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {return}
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
+    }
 }
 
